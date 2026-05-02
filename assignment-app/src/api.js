54f0fa3b264bf7dfa385707f94b70318b1d91a2c
Leela <reddileelasyamala@gmail.com>
2026-05-02 23:30:28 +0530

    connected frontend to live backend

--- assignment-app/src/api.js
+++ assignment-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://assignment-backend-123.onrender.com"
 });
 
 export default API;
--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'assignment_db',
-  password: '1234', // Replace with your real password
-  port: 5432,
+  connectionString: "postgresql://neondb_owner:npg_edHOCgUIuV94@ep-jolly-silence-aogk30ve-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
